e28dd5938cd2e6ceaec7fff04a4426fd60725c80
Alex <didyshen.oleksandr@gmail.com>
2022-07-12 21:26:10 +0300

    Add funciton to play one round

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ function computerPlay() {
     const items = ["Rock", "Paper", "Scissors"];
     let randomItem = items[Math.floor(Math.random() * items.length)];
     return randomItem;
+} 
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay();
+    playerSelection = playerSelection.toLowerCase();
+    let caseInsensitivePlayer = playerSelection[0].toUpperCase() + 
+        playerSelection.slice(1)
+    
+    if (
+        computerSelection === "Rock" && playerSelection === "scissors" ||
+        computerSelection === "Paper" && playerSelection === "rock" || 
+        computerSelection === "Scissors" && playerSelection === "paper"
+        ) {
+        return `Computer won! ${computerSelection} beats ${caseInsensitivePlayer}.`
+    } else if (
+        computerSelection === playerSelection
+        ) {
+            return `It's a tie, you and computer chose ${computerSelection}.`
+        } else {
+            return `You won! ${caseInsensitivePlayer} beats ${computerSelection}.`
+        }
 }
