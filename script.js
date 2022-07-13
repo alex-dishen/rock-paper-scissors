e5e4a8156d2ac4350af9a46f0bc86f7c76fdd771
Alex <didyshen.oleksandr@gmail.com>
2022-07-13 07:45:52 +0300

    Add function to play 5 rounds

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Function that randomly returns rock paper or scissors
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
     const items = ["Rock", "Paper", "Scissors"];
     let randomItem = items[Math.floor(Math.random() * items.length)];
     return randomItem;
 } 
 
-function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-    let caseInsensitivePlayer = playerSelection[0].toUpperCase() + 
-        playerSelection.slice(1)
-        
+function playRound(playerSelection, computerSelection) {        
     if (playerSelection != null) {
         playerSelection = playerSelection.toLowerCase();
     }
@@ -19,13 +18,17 @@ function playRound(playerSelection, computerSelection) {
         computerSelection === "Paper" && playerSelection === "rock" || 
         computerSelection === "Scissors" && playerSelection === "paper"
         ) {
-        return `Computer won! ${computerSelection} beats ${caseInsensitivePlayer}.`
+        computerScore++
+
+        return `${computerSelection} beats ${playerSelection}.`
     } else if (
         playerSelection === "rock" && computerSelection === "Scissors" ||
         playerSelection === "paper" && computerSelection === "Rock" || 
         playerSelection === "scissors" && computerSelection === "Paper"
         ) {
-            return `You won! ${caseInsensitivePlayer} beats ${computerSelection}.`
+            playerScore++
+
+            return `${playerSelection} beats ${computerSelection}.`
         } else if (
             playerSelection === computerSelection.toLowerCase()
         ) {
@@ -34,3 +37,25 @@ function playRound(playerSelection, computerSelection) {
             return `You didn't choose an item to fight with`
         }
 }
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper or Scissors?")
+        let computerSelection = computerPlay();        
+
+        console.log(`You chose: ${playerSelection}`);
+        console.log(`Computer chose: ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Score: \nYou: ${playerScore} Computer: ${computerScore}`)
+        console.log("-------------------------");
+    }
+    if (playerScore > computerScore) {
+        console.log("You won!!!!!")
+    } else if (computerScore > playerScore){
+        console.log("Computer won!!!!!!!")
+    } else {
+        console.log("Friendship WON!!!!!")
+    }
+}
+
+game()
