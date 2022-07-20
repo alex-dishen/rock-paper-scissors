c6774c319e88f5800a0aaf6a0d24ce364a19b324
Alex <didyshen.oleksandr@gmail.com>
2022-07-20 09:00:25 +0300

    Change logic from all button to single one
    I want to add another button when player wins or looses the game to
    reload the page. When logic was focused on all buttons the button would
    play a single round before reloading the page. Now when you click any
    other buttons they won't play the game any more.

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ let computerScore = 0;
 let roundPlayer = '';
 
 function computerPlay() {
-    const items = ["Rock", "Paper", "Scissors"];
-    let randomItem = items[Math.floor(Math.random() * items.length)];
-    return randomItem;
+    const items = ['Rock', 'Paper', 'Scissors'];
+    return items[Math.floor(Math.random() * items.length)];
 } 
 
 function playRound(playerSelection, computerSelection) {
@@ -13,86 +12,87 @@ function playRound(playerSelection, computerSelection) {
                       playerSelection.slice(1);
 
     if (
-        playerSelection === "Rock" && computerSelection === "Scissors" ||
-        playerSelection === "Paper" && computerSelection === "Rock" || 
-        playerSelection === "Scissors" && computerSelection === "Paper"
+        playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+        playerSelection === 'Paper' && computerSelection === 'Rock' || 
+        playerSelection === 'Scissors' && computerSelection === 'Paper'
         ) {
-            playerScore++
-            roundPlayer = "player"
+            playerScore++;
+            roundPlayer = 'player';
         
-            return `${playerSelection} beats ${computerSelection}.`
+            return `${playerSelection} beats ${computerSelection}.`;
 
     } else if (
-        computerSelection === "Rock" && playerSelection === "Scissors" ||
-        computerSelection === "Paper" && playerSelection === "Rock" || 
-        computerSelection === "Scissors" && playerSelection === "Paper"
+        computerSelection === 'Rock' && playerSelection === 'Scissors' ||
+        computerSelection === 'Paper' && playerSelection === 'Rock' || 
+        computerSelection === 'Scissors' && playerSelection === 'Paper'
         ) {
-            computerScore++
-            roundPlayer = 'computer'
+            computerScore++;
+            roundPlayer = 'computer';
 
-            return `${playerSelection} is beaten by ${computerSelection}.`
+            return `${playerSelection} is beaten by ${computerSelection}.`;
         } else if (
             playerSelection === computerSelection
         ) {
-            roundPlayer = "tie"
+            roundPlayer = 'tie';
 
-            return `You and computer chose ${computerSelection}.`
+            return `You and computer chose ${computerSelection}.`;
         } 
 }
 
+const scoreInfo = document.querySelector('.scoreinfo');
+const scoreMessage = document.querySelector('.scoremessage');
+const pcChoice = document.querySelector('.pcsign');
+const playerChoice = document.querySelector('.playersign');
+const pcScore = document.querySelector('.pcscore');
+const userScore = document.querySelector('.playerscore');
+const btnRock = document.querySelector('.rock');
+const btnPaper = document.querySelector('.paper');
+const btnScissors = document.querySelector('.scissors');
+
 function setWinner() {
-  if (roundPlayer === "player") {
-          return "You won!";
-      } else if (roundPlayer === "computer"){
-          return "Computer won!";
+  if (roundPlayer === 'player') {
+          return 'You won!';
+      } else if (roundPlayer === 'computer'){
+          return 'Computer won!';
       } else {
-          return "It's a tie";
+          return 'It\'s a tie';
       }
 }
 
-function game() {
-    const scoreInfo = document.querySelector('.scoreinfo');
-    const scoreMessage = document.querySelector('.scoremessage');
-    const pcChoice = document.querySelector('.pcsign');
-    const playerChoice = document.querySelector('.playersign');
-    const pcScore = document.querySelector('.pcscore');
-    const userScore = document.querySelector('.playerscore');
-    const btn = document.querySelectorAll('button');
+function play(playerSelection) {
+    let computerSelection = computerPlay();
+    scoreMessage.textContent = playRound(playerSelection, computerSelection);
+    scoreInfo.textContent = setWinner();
+    
+    switch (playerSelection) {
+        case "rock":
+            playerChoice.setAttribute("src", "./img/rock-hand.png");
+            break;
+        case "paper": 
+            playerChoice.setAttribute("src", "./img/paper-hand.png");
+            break;
+        case "scissors":
+            playerChoice.setAttribute("src", "./img/scissors-hand.png");
+            break;
+    }
+    switch (computerSelection) {
+        case "Rock":
+            pcChoice.setAttribute("src", "./img/rock-hand.png");
+            break;
+        case "Paper": 
+            pcChoice.setAttribute("src", "./img/paper-hand.png");
+            break;
+        case "Scissors":
+            pcChoice.setAttribute("src", "./img/scissors-hand.png");
+            break;
+    }
 
-    btn.forEach((button) => {
-        let playerSelection = button.className;
-        button.addEventListener('click', () => {
-                let computerSelection = computerPlay();
-                scoreMessage.textContent = playRound(playerSelection, computerSelection);
-                scoreInfo.textContent = setWinner();
-                switch (playerSelection) {
-                    case "rock":
-                        playerChoice.setAttribute("src", "./img/rock-hand.png");
-                        break;
-                    case "paper": 
-                        playerChoice.setAttribute("src", "./img/paper-hand.png");
-                        break;
-                    case "scissors":
-                        playerChoice.setAttribute("src", "./img/scissors-hand.png");
-                        break;
-                }
-
-                switch (computerSelection) {
-                    case "Rock":
-                        pcChoice.setAttribute("src", "./img/rock-hand.png");
-                        break;
-                    case "Paper": 
-                        pcChoice.setAttribute("src", "./img/paper-hand.png");
-                        break;
-                    case "Scissors":
-                        pcChoice.setAttribute("src", "./img/scissors-hand.png");
-                        break;
-                }
-                pcScore.textContent = `Computer: ${computerScore}`;
-                userScore.textContent = `Player: ${playerScore}`;
-            }
-        )
-    })
+    pcScore.textContent = `Computer: ${computerScore}`;
+    userScore.textContent = `Player: ${playerScore}`;
 }
 
-game()
+btnRock.addEventListener('click', () => {play('rock');})
+
+btnPaper.addEventListener('click', () => {play('paper')})
+
+btnScissors.addEventListener('click', () => {play('scissors')})
